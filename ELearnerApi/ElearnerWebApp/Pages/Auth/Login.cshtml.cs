cac43a0a1c8900b165d765c4d8c0a9bf0e99d0df
Huy <huydnqse151303@fpt.edu.vn>
2022-10-15 22:48:00 +0700

    done Topics, wip login

---
+++ ELearnerApi/ElearnerWebApp/Pages/Auth/Login.cshtml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ELearnerApi.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+
+namespace ElearnerWebApp.Pages.Auth
+{
+    public class LoginModel : PageModel
+    {
+        private readonly ELearnerApi.Models.ElearnnerDBContext _context;
+
+        public LoginModel(ELearnerApi.Models.ElearnnerDBContext context)
+        {
+            _context = context;
+        }
+
+        private List<Role> GenerateRoleName()
+        {
+            List<Role> roles = new List<Role>();
+            roles.Add(new Role()
+            {
+                Id = 1,
+                RoleName = "user"
+            });
+
+            roles.Add(new Role()
+            {
+                Id = 2,
+                RoleName = "admin"
+            });
+
+            roles.Add(new Role()
+            {
+                Id = 3,
+                RoleName = "teacher"
+            });
+
+            return roles;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["TopicId"] = new SelectList(_context.Topics, "Id", "ImgUrl");
+            return Page();
+        }
+
+        [BindProperty]
+        public Account Account { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var user = _context.Accounts.Where(acc => acc.Email == Account.Email && acc.Password == Account.Password).FirstOrDefault();
+            if(user != null )
+            {
+
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, Account.Email),
+                    new Claim(ClaimTypes.Role, Account.Role),
+
+                };
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+                var authProperties = new AuthenticationProperties
+                {
+
+                };
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(principal), authProperties);
+
+                if (user.Role == "admin")
+                {
+                    return RedirectToPage("/Topics/Index");
+                }
+            }
+            else
+            {
+
+            }
+
+
+            _context.Accounts.Add(Account);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ELearnerApi/ElearnerWebApp/Pages/Auth/Logout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ElearnerWebApp.Pages.Auth
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
