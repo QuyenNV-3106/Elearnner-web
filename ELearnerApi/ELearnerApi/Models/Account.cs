a8343fb1f78cdef815a4c3638ad20ca4c52503e9
Quyen Nguyen <band3106@gmail.com>
2022-10-15 17:46:55 +0700

    push

---
+++ ELearnerApi/ELearnerApi/Models/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ELearnerApi.Models
+{
+    public partial class Account
+    {
+        public Account()
+        {
+            Vocabularies = new HashSet<Vocabulary>();
+        }
+
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public int? TopicId { get; set; }
+        public string Role { get; set; }
+
+        public virtual Topic Topic { get; set; }
+        public virtual ICollection<Vocabulary> Vocabularies { get; set; }
+    }
+}
